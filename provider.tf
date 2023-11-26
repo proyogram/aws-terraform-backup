ccef13f271ea90d837a19ef9faae0c7e71b591c3
proyogram <proyogram@gmail.com>
2023-11-27 00:41:48 +0900

    tf-verを1.4.0に変更

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.4.0"
   backend "s3" {
     bucket  = "tf-backend-proyogram"
     region  = "ap-northeast-1"
